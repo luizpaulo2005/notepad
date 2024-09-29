0ab4270fbcbbb8df0ef5d4213032c97a5702370f
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2024-09-29 00:54:04 -0300

    feat: add environment variable validation and update next-auth configuration

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,35 @@
-import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import { prisma } from "./prisma";
+import { env } from "./env";
  
-const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
-})
+const authOptions = {
+  secret: env.NEXT_PUBLIC_AUTH_SECRET,
+  providers: [Google({
+    clientId: env.NEXT_PUBLIC_GOOGLE_AUTH_CLIENT_ID,
+    clientSecret: env.NEXT_PUBLIC_GOOGLE_AUTH_CLIENT_SECRET,
+  })],
+  callbacks: {
+    // @ts-expect-error - type error
+    async signIn({ account, profile }) {
+      if (account.provider === "google" && profile.email_verified) {
+        const select = await prisma.user.findUnique({
+          where: { email: profile.email },
+        });
 
-export { handlers, signIn, signOut, auth }
+        if (!select) {
+          await prisma.user.create({
+            data: {
+              email: profile.email,
+              name: profile.name,
+              imageUrl: profile.picture,
+            },
+          });
+        }
+      }
+
+      return true;
+    },
+  },
+}
+
+export { authOptions }
