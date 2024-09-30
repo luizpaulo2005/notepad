0c35e9e18d3f430a103272d3db95948516f9bd5a
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2024-09-30 15:14:58 -0300

    feat: add delete note button to note component

--- src/components/notes/note.tsx
+++ src/components/notes/note.tsx
@@ -1,30 +1,37 @@
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import ptBR from "dayjs/locale/pt-br";
+import { DeleteNote } from "./delete-note";
 
 dayjs.extend(relativeTime);
 dayjs.locale(ptBR);
 
 interface NoteProps {
+  id: string
   title: string;
   content: string;
   userId: string;
   createdAt: Date;
 }
 
-const Note = ({ title, content, createdAt }: NoteProps) => {
+const Note = ({ id, title, content, createdAt }: NoteProps) => {
   return (
-    <div className="min-w-64 max-w-96 max-h-96 flex flex-col gap-4">
-      <div className="bg-zinc-300 dark:bg-zinc-800 h-48 p-4 rounded-lg shadow-md flex flex-col justify-between">
+    <div className="min-w-64 max-w-96 max-h-[400px] flex flex-col gap-4">
+      <div className="bg-zinc-300 dark:bg-zinc-800 h-56 p-4 rounded-lg shadow-md flex flex-col justify-between">
         <div>
           <h3 className="text-lg font-semibold">{title}</h3>
           <p className="text-sm dark:text-gray-500 overflow-hidden break-words line-clamp-6">
             {content}
           </p>
         </div>
+        <div className="flex items-center justify-between gap-2">
         <p className="text-xs dark:text-gray-400">
           Criado {dayjs().to(createdAt)}
         </p>
+        <div>
+          <DeleteNote id={id} />
+        </div>
+        </div>
       </div>
     </div>
   );
