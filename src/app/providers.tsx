26ff301dbaf13eddfdfec0755b37f609d0665c54
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2024-09-30 00:32:06 -0300

    feat: add QueryProvider provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,6 +2,8 @@
 
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider as ColorProvider } from "next-themes";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/lib/react-query";
 import type { ThemeProviderProps } from "next-themes/dist/types";
 import type { ReactNode } from "react";
 
@@ -13,4 +15,10 @@ const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
   return <ColorProvider {...props}>{children}</ColorProvider>;
 };
 
-export { AuthProvider, ThemeProvider };
+const QueryProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export { AuthProvider, QueryProvider, ThemeProvider };
