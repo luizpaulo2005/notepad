16f0c667e895b9348c8c0023aca6f509387c4426
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2024-09-30 10:31:03 -0300

    feat: add note create method and validation

--- src/app/api/note/route.ts
+++ src/app/api/note/route.ts
@@ -2,6 +2,7 @@ import { authOptions } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const getNotes = async (req: NextRequest) => {
@@ -46,5 +47,54 @@ const getNotes = async (req: NextRequest) => {
   }
 };
 
-export { getNotes as GET };
- 
+const createNoteSchema = z.object({
+  title: z.string().nonempty("O campo título não pode ser vazio").max(32),
+  content: z
+    .string()
+    .nonempty("O campo conteúdo não pode ser vazio")
+    .max(228, "Excedeu o limite de caracteres"),
+});
+
+const createNote = async (req: NextRequest) => {
+  try {
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        // @ts-expect-error - type issue
+        email: session.user.email,
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const { title, content } = createNoteSchema.parse(await req.json());
+
+    const create = await prisma.note.create({
+      data: {
+        title,
+        content,
+        userId: user.id,
+      },
+    });
+
+    if (!create) {
+      return NextResponse.json({ error: "Note not created" }, { status: 500 });
+    }
+
+    return NextResponse.json(create, { status: 201 });
+  } catch (err) {
+    return NextResponse.json(
+      { error: `"Internal Server Error": ${err}` },
+      { status: 500 }
+    );
+  }
+};
+
+export { getNotes as GET, createNote as POST };
