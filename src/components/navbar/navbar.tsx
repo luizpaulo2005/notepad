48c083c6513025315aedad883b761b0f9b9ba7db
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2024-09-30 00:32:12 -0300

    style: add margin-bottom to navbar container

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -3,7 +3,7 @@ import { User } from "@/components/user/user";
 
 const Navbar = () => {
   return (
-    <div className="flex items-center justify-between">
+    <div className="flex items-center justify-between mb-2">
       <div className="flex items-center gap-2">
         <NotebookText className="size-6 hidden lg:block" />
         note.pad
