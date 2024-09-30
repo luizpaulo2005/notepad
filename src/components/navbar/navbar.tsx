5b3828b7132ec89502e72c7e7e0b5c7f1ebc9089
luizpaulo2005 <luizpaulorodrigues2005@gmail.com>
2024-09-30 18:00:41 -0300

    refactor: replace NotebookText component with Image in Navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,11 +1,11 @@
-import { NotebookText } from "lucide-react";
 import { User } from "@/components/user/user";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between mb-2">
       <div className="flex items-center gap-2">
-        <NotebookText className="size-6 hidden lg:block" />
+        <Image src="/icon.png" alt="Logo" width={32} height={32} />
         note.pad
       </div>
       <User />
